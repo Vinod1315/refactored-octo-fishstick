c062b1868c2094fb72c918dec642593f30ee2f98
Sildus <09wrists.dormer@icloud.com>
2024-10-11 17:42:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -97,7 +97,6 @@ function checkGuessForDraw(team1, team2) {
       displayFinalMessage("Unlucky with the year but Congratulations! See you tomorrow for another classic match.");
     }
   });
-});
 
 // Variables for game logic
 const maxAttempts = 5;
